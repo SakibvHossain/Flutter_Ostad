a1b730a6b1437faa97d92c7a15aee768be732975
Sakib Hossain <92059000+SakibvHossain@users.noreply.github.com>
2024-06-28 14:40:00 +0600

    update: Update test.dart

--- Module_5/Test/test.dart
+++ Module_5/Test/test.dart
@@ -1,1 +1,23 @@
+class Media{
+  void play(){
+    print('Playing media');
+  }
+}
 
+class Song extends Media{
+  late String artist;
+  Song(this.artist);
+
+  @override
+  void play(){
+    print('Playing song by $artist');
+  }
+}
+
+void main(){
+  Media media1 = Song('Anna');
+  media1.play();
+
+  Media media2 = Song('Miya');
+  media2.play();
+}
